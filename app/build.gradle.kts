5474d6f4554dd350111e889b803b37633b8c87e9
doanhungthinh <22115053122345@sv.ute.udn.vn>
2024-12-21 09:59:07 +0700

    lan2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
+
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -33,11 +35,28 @@ android {
 
 dependencies {
 
-    implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation ("com.google.android.gms:play-services-auth:20.6.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("com.google.android.gms:play-services-basement:18.4.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation ("com.facebook.android:facebook-login:latest.release")
+    implementation("com.google.firebase:firebase-bom:latest.release")
+    implementation ("com.google.firebase:firebase-auth:21.0.8")
+    implementation ("com.facebook.android:facebook-android-sdk:latest.release")
+    implementation ("com.google.firebase:firebase-database:20.3.0")
+    implementation ("com.firebaseui:firebase-ui-database:latest.release")
+    implementation("com.google.firebase:firebase-analytics:21.3.0")
+    implementation("com.google.firebase:firebase-core:latest.release")
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
 }
